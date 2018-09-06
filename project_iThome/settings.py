4ddcfb6bf440ad94dbc6e0b5f4c0b21ae00f6f46
Frank Ye(葉詠富) <frank_ye@asus.com>
2018-09-06 21:04:31 +0800

    make it better

--- project_iThome/settings.py
+++ project_iThome/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z#6y!iw)ejguy(inp@r@i_hl_wyteb34zh6%vx()4i@-3g=n_-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chatbot',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Taipei'
 
 USE_I18N = True
 
