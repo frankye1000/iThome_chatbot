ce37fa386561f4c4e9bc7f7c2cb78a08d7d35e35
Frank Ye(葉詠富) <frank_ye@asus.com>
2018-09-11 21:25:42 +0800

    make it better

--- chatbot/views.py
+++ chatbot/views.py
@@ -15,15 +15,11 @@ scheduler.add_jobstore(DjangoJobStore(), "default")
 
 scheduler.start()
 
-def time_task(task):
+def time_task():
     print("I'm a test job!")
 
 scheduler.add_job(time_task, "cron", second=2, misfire_grace_time=30)
 register_events(scheduler)
-
-
-
-
 
 
 
