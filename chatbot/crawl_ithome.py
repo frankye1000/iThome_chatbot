b1127a39ad2e25e29bbd5f169ebb24afdde2b058
frankye1000 <33119231+frankye1000@users.noreply.github.com>
2018-09-12 23:09:16 +0800

    Update crawl_ithome.py

--- chatbot/crawl_ithome.py
+++ chatbot/crawl_ithome.py
@@ -1,13 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from datetime import date
-# import pymssql
-# conn = pymssql.connect(server='127.0.0.1:1433',
-#                        user='SA',
-#                        password='Asus#1234',
-#                        database='SideProject',
-#                        charset="utf8")
-# cursor = conn.cursor()
+
 
 today = date.today()
 today = today.strftime("%Y-%m-%d")
@@ -35,13 +29,3 @@ def crawl_ithome():
 
     return re
 
-# ## "依序"一次插入一筆資料
-# count=1
-# insert_data = """INSERT INTO IThome VALUES (%s, %s)"""
-# for d in IThome_data:
-#     cursor.execute(insert_data,d)
-#     print(count,d)
-#     count+=1
-# # 執行SQL動作
-# conn.commit()
-# conn.close()
